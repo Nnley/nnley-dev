fb08a987e0ee9b4b49d5223ee2d74e2b3661729e
Nnley <kragelek86@gmail.com>
2024-11-14 13:15:29 +0300

    Added a video to bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,43 @@ export default function Home() {
 	useAnimatedTitle()
 
 	const [mounted, setMounted] = React.useState(false)
+	const [isPlaying, setIsPlaying] = React.useState(true)
+	const videoRef = React.useRef<HTMLVideoElement | null>(null)
+
+	const enableSound = () => {
+		if (videoRef.current) {
+			videoRef.current.muted = false
+			videoRef.current.volume = 0.1
+		}
+
+		document.removeEventListener('click', enableSound)
+		document.removeEventListener('keydown', enableSound)
+	}
+
+	React.useEffect(() => {
+		document.addEventListener('click', enableSound)
+		document.addEventListener('keydown', enableSound)
+
+		return () => {
+			document.removeEventListener('click', enableSound)
+			document.removeEventListener('keydown', enableSound)
+		}
+	}, [])
 
 	React.useEffect(() => {
 		setMounted(true)
 	}, [])
+
+	const handlePlayPause = () => {
+		if (videoRef.current) {
+			if (isPlaying) {
+				videoRef.current.pause()
+			} else {
+				videoRef.current.play()
+			}
+			setIsPlaying(!isPlaying)
+		}
+	}
 
 	if (!mounted) {
 		return null
@@ -19,10 +52,21 @@ export default function Home() {
 
 	return (
 		<>
-			<main className='flex items-center justify-center'></main>
+			<main className='flex items-center justify-center bg-black'>
+				<video
+					ref={videoRef}
+					autoPlay
+					muted
+					loop
+					playsInline
+					className='w-screen h-screen object-cover overflow-hidden opacity-20'
+				>
+					<source src='/videos/Von-dutch-1080p.webm' type='video/webm' />
+				</video>
+			</main>
 			<footer className='fixed bottom-0 w-full text-center p-4'>
-				<SocialNetworksDock />
-				<p className='text-gray-300 dark:text-gray-500 mt-4'>© {new Date().getFullYear()} Nnley</p>
+				<SocialNetworksDock handlePlayPause={handlePlayPause} playingVideo={isPlaying} />
+				<p className='text-gray-400 dark:text-gray-500 mt-4'>© {new Date().getFullYear()} Nnley</p>
 			</footer>
 		</>
 	)

daa33fe5e7e9ef2c380a2101d714479491b8f248
Nnley <kragelek86@gmail.com>
2024-11-15 20:12:42 +0200

    Removed preload video

--- src/components/shared/loading-page.tsx
+++ src/components/shared/loading-page.tsx
@@ -24,8 +24,6 @@ export const LoadingPage: React.FC<Props> = ({ setIsLoadingPage }) => {
 	const [isLong] = useTimeout(10000)
 	const [isLoadedPageTimeout] = useTimeout(1000) // Just so the page doesn't blink
 
-	const [isVideoContentLoaded, setIsVideoContentLoaded] = React.useState(false)
-
 	const { videoSrc, isLoadingVideo, setIsLoadingVideo, setVideoSrc } = useVideoSourceStore(state => state)
 	const videoRef = React.useRef<HTMLVideoElement>(null)
 	const updateIsMobile = useDeviceInfoStore(state => state.updateIsMobile)
@@ -39,11 +37,11 @@ export const LoadingPage: React.FC<Props> = ({ setIsLoadingPage }) => {
 	React.useEffect(() => {
 		if (!speed) return
 
-		if (speed > 8000) {
+		if (speed > 12000) {
 			setVideoSrc(videoQualities['1080p'])
-		} else if (speed > 3000) {
+		} else if (speed > 5000) {
 			setVideoSrc(videoQualities['720p'])
-		} else if (speed > 1000) {
+		} else if (speed > 1500) {
 			setVideoSrc(videoQualities['480p'])
 		} else {
 			setVideoSrc('')
@@ -52,62 +50,30 @@ export const LoadingPage: React.FC<Props> = ({ setIsLoadingPage }) => {
 		setIsLoadingVideo(false)
 	}, [speed])
 
-	const handleProgress = () => {
-		if (videoRef.current) {
-			const buffered = videoRef.current.buffered
-
-			if (buffered.length > 0) {
-				const loaded = buffered.end(buffered.length - 1)
-
-				if (loaded >= 5) {
-					setIsVideoContentLoaded(true)
-				}
-			}
-		}
-	}
-
-	React.useEffect(() => {
-		if (videoRef.current) {
-			if (videoRef.current.readyState === 4) {
-				return setIsVideoContentLoaded(true)
-			}
-
-			videoRef.current.addEventListener('progress', handleProgress)
-			videoRef.current.addEventListener('loadeddata', handleProgress)
-		}
-
-		return () => {
-			if (videoRef.current) {
-				videoRef.current.removeEventListener('progress', handleProgress)
-				videoRef.current.removeEventListener('loadeddata', handleProgress)
-			}
-		}
-	}, [videoSrc, videoRef.current])
-
 	return (
 		<main className='relative flex items-center justify-center w-dvh h-dvh overflow-hidden'>
 			<Meteors number={30} />
 			<div className='flex flex-col items-center'>
-				{!isLoadingVideo && isLoadedPageTimeout() && isVideoContentLoaded ? (
+				{!isLoadingVideo && isLoadedPageTimeout() ? (
 					<BlurIn className='font-bold text-black dark:text-white' word='PAGE LOADED!' />
 				) : (
 					<HyperText className='text-4xl font-bold text-black dark:text-white' text='LOADING PAGE...' />
 				)}
 
-				{!isLoadingVideo && isLoadedPageTimeout() && isVideoContentLoaded && (
+				{!isLoadingVideo && isLoadedPageTimeout() && (
 					<ShinyButton className='w-64 py-5 rounded-xl mt-7' onClick={() => setIsLoadingPage(false)}>
 						<h2 className='text-xl font-bold text-black dark:text-white'>LET'S GOOOO</h2>
 					</ShinyButton>
 				)}
 
-				{isLong() && isLoadingVideo && !isVideoContentLoaded && (
+				{isLong() && isLoadingVideo && (
 					<TypingAnimation
 						className={cn('text-xl font-bold text-black dark:text-white')}
 						text="What's taking so long?"
 					/>
 				)}
 
-				{speed && speed < 700 && (
+				{speed && speed < 1500 && (
 					<BlurIn
 						className={cn('font-normal text-black/30 dark:text-white/30 mt-7 max-w-xs')}
 						word={<p className='text-lg'>(I suggest coming back here when your internet connection gets better!)</p>}

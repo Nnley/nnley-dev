3cddc4a1a2b12f84752a5fbc76c32c3bde142262
Nnley <kragelek86@gmail.com>
2024-11-15 16:49:03 +0200

    Fix caching & state

--- src/hooks/use-connection-seed.ts
+++ src/hooks/use-connection-seed.ts
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react'
 
 export const useConnectionSpeed = () => {
-	const [speed, setSpeed] = useState(0)
+	const [speed, setSpeed] = useState<number | null>(null)
 
 	useEffect(() => {
 		const measureConnectionSpeed = async () => {
 			const startTime = new Date().getTime()
-			const response = await fetch('/500KB.bin')
+			const response = await fetch('/500KB.bin', { cache: 'no-store' })
 			const blob = await response.blob()
 			const endTime = new Date().getTime()
 			const fileSize = blob.size
 			const duration = (endTime - startTime) / 1000
-			const speed = ((fileSize / duration) * 8) / 1024
+			const calculatedSpeed = ((fileSize / duration) * 8) / 512
 
-			setSpeed(speed)
+			setSpeed(calculatedSpeed)
 		}
 
 		measureConnectionSpeed()
